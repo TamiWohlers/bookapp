31b45f278433caf0f174f6244ad89d2891d508a2
Tami Wohlers <tamiwohelrs28@yahoo.com>
2015-08-14 19:39:43 -0600

    stars working

--- bookapp/Controllers/homeCtrl.js
+++ bookapp/Controllers/homeCtrl.js
@@ -1,6 +1,26 @@
 var app = angular.module('bookApp');
 
-app.controller('homeCtrl', function($scope, homeService){
-  $scope.author = 'Charles Dickens';
-  $scope.currentFaves = homeService.currentFaves;
+app.controller('homeCtrl', function($scope, homeService) {
+    $scope.author = 'Charles Dickens';
+    $scope.currentFaves = homeService.currentFaves;
+    $scope.rateFunction = function(rating) {
+    	console.log('at homeCtrl');
+        // var _url = 'https:/Services/homeService';
+
+        // var data = {
+        //     rating: rating
+        // };
+
+        // $http.post(_url, angular.toJson(data), {
+        //         cache: false
+        //     })
+        //     .success(function(data) {
+        //         success(data);
+        //     })
+        //     .error(function(data) {
+        //         error(data);
+        //     });
+
+    };
+
 });
--- bookapp/Services/homeService.js
+++ bookapp/Services/homeService.js
@@ -2,13 +2,16 @@ app.service('homeService', function() {
 	this.currentFaves = [
 	  {title: "The Nightingale",
 	  	author: "Kristin Hannah",
-	    image:'https://d.gr-assets.com/books/1422580224s/21853621.jpg'},
+	    image:'https://d.gr-assets.com/books/1422580224s/21853621.jpg',
+	    rating: 1},
 	  {title: "Big, Little Lies", 
 	    author: "Liane Moriarty",
-	    image:'https://d.gr-assets.com/books/1399582436s/19486412.jpg'},
+	    image:'https://d.gr-assets.com/books/1399582436s/19486412.jpg',
+	    rating: 1},
 	   {title: "Girl on the Trian",
 	     author: "Paula Hawkins",
-	     image:'https://d.gr-assets.com/books/1421709869s/22557272.jpg'}
+	     image:'https://d.gr-assets.com/books/1421709869s/22557272.jpg',
+		rating:1}
 	]
 
 	
